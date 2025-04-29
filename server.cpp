207fb4ab3d1ac885e3fb2c4b43c8186de90070ad
Stefan <stefan.ghinescu2307@stud.acs.upb.ro>
2025-04-29 19:33:11 +0000

    idk

---
+++ server.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int port = atoi(argv[1]);
+
+    return 0;
+}
